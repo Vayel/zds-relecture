96400866223d39793f312d15b435452da2a92064
Vincent Lefoulon <vincent.lefoulon@free.fr>
2018-11-17 13:52:22 +0000

    Make feedback areas follow the scroll

--- edit_ui.js
+++ edit_ui.js
@@ -7,6 +7,12 @@ function readTitle() {
     return readText(h1);
 }
 
+function getAbsoluteTop(element) {
+    var bodyRect = document.body.getBoundingClientRect(),
+        elemRect = element.getBoundingClientRect();
+    return elemRect.top - bodyRect.top;
+}
+
 var content = document.getElementById("content");
 var wrapper = document.querySelector(".content-wrapper");
 var FEEDBACK_WRAPPER_ID = "feedback-wrapper";
@@ -17,7 +23,7 @@ var FEEDBACK_WRAPPER_ID = "feedback-wrapper";
  */
 (function(content) {
     var classes = content.className.split(' ');
-    if(classes.indexOf("zen-mode") != -1)
+    if (classes.indexOf("zen-mode") != -1)
         return;
     document.querySelector(".open-zen-mode").click();
 })(content);
@@ -40,14 +46,13 @@ wrapper.setAttribute("style", (
  * Create feedback wrapper
  */
 (function(content) {
-    if(content.querySelector("#" + FEEDBACK_WRAPPER_ID))
+    if (content.querySelector("#" + FEEDBACK_WRAPPER_ID))
         return;
 
     var div = document.createElement(FEEDBACK_WRAPPER_ID);
     div.id = FEEDBACK_WRAPPER_ID;
     div.style.display = "inline-block";
     div.style.verticalAlign = "top";
-    div.style.width = "45%";
     content.appendChild(div);
 })(content);
 
@@ -55,14 +60,21 @@ wrapper.setAttribute("style", (
 /*
  * Create feedback areas 
  */
+function getFeedbackAreaId(sectionId) {
+    return sectionId + "-feedback";
+}
+
 function createFeedbackArea(wrapper, section) {
-    var id = section.id + "-feedback";
+    var id = getFeedbackAreaId(section.id);
     // We use this syntax because ids contain numbers
-    if(wrapper.querySelector("[id='" + id + "']"))
+    if (wrapper.querySelector("[id='" + id + "']"))
         return;
 
     var div = document.createElement("div");
     div.id = id;
+    div.style.position = "fixed";
+    div.style.top = "30px";
+    div.style.width = "45%";
 
     var label = document.createElement("div");
     label.innerHTML = section.title;
@@ -70,27 +82,66 @@ function createFeedbackArea(wrapper, section) {
     div.appendChild(label);
 
     var textarea = document.createElement("textarea");
+    textarea.style.height = (window.innerHeight * 0.5) + "px";
     div.appendChild(textarea);
 
     wrapper.appendChild(div);
+    section.feedbackWrapper = div;
 }
 
 var feedbackWrapper = document.getElementById(FEEDBACK_WRAPPER_ID);
 var sections = (function() {
     var titles = document.querySelectorAll("h2:not(.subtitle)");
-    var sections = {};
+    var sections = [{
+        id: "introduction",
+        title: readTitle(),
+        titleElement: document.querySelector("h1"),
+        url: window.location,
+        top: getAbsoluteTop(document.querySelector("h1")),
+    }];
+
     var link;
-    for(title of titles) {
+    for (title of titles) {
         link = title.querySelector("a");
-        sections[title.id] = {
+        sections.push({
             id: title.id,
             title: readText(link),
+            titleElement: link,
             url: link.href,
-        };
+            top: getAbsoluteTop(link),
+        });
     }
     return sections;
 })();
 
-for(var id in sections) {
-    createFeedbackArea(feedbackWrapper, sections[id]); 
+for (var section of sections) {
+    createFeedbackArea(feedbackWrapper, section); 
 }
+
+function displayFeedbackAreas() {
+    var scrollTop = (((t = document.documentElement) || (t = document.body.parentNode))
+                     && typeof t.scrollTop == 'number' ? t : document.body).scrollTop;
+    var section, relYCur, relYNext, height;
+    var MARGIN = 20;
+
+    for (var i=0; i < sections.length; i++) {
+        section = sections[i];
+        relYCur = section.top - scrollTop;
+        relYNext = (sections[i+1] !== undefined) ? (sections[i+1].top - scrollTop) : Infinity;
+        height = section.feedbackWrapper.getBoundingClientRect().height;
+
+        if (relYCur > 0 && relYCur < window.innerHeight) {
+            // The section title appears on the screen
+            section.feedbackWrapper.style.visibility = "visible";
+            section.feedbackWrapper.style.top = relYCur + "px";
+        } else if (relYCur < 0 && relYNext - (height + MARGIN) > 0) {
+            // We are between the section title and the next section title
+            section.feedbackWrapper.style.visibility = "visible";
+        } else {
+            section.feedbackWrapper.style.visibility = "hidden";
+        }
+    }
+}
+
+document.addEventListener("scroll", displayFeedbackAreas);
+displayFeedbackAreas();
