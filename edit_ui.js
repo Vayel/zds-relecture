8223dd6902b293328f0f7b634eb44d7459f830af
Vincent Lefoulon <vincent.lefoulon@free.fr>
2018-11-17 11:18:02 +0000

    Create textareas

--- edit_ui.js
+++ edit_ui.js
@@ -1,8 +1,3 @@
-function createTextarea() {
-    var el = document.createElement("textarea");
-    return el;
-}
-
 function readText(el) {
     return el.innerText || el.textContent;
 }
@@ -12,36 +7,90 @@ function readTitle() {
     return readText(h1);
 }
 
-function readSections() {
-    var els = document.querySelectorAll("h2:not(.subtitle)");
-    var sections = [];
-    var link;
-    for(el of els) {
-        link = el.querySelector("a");
-        sections.push({
-            id: el.id,
-            text: readText(link),
-            url: link.href,
-        });
-    }
-    return sections;
-}
-
-function openZenMode(content) {
-    var classes = content.className.split(' ');
-    if(classes.indexOf("zen-mode") != -1) return;
-    var btn = document.querySelector(".open-zen-mode");
-    btn.click();
-}
-
-function formatContent(wrapper) {
-    // We need to use `el.setAttribute()` instead of `el.style.property`
-    // because there is an "!important" CSS rule that we need to override.
-    wrapper.setAttribute("style", "width: 45%; margin: 0px 30px !important;");
-}
-
 var content = document.getElementById("content");
 var wrapper = document.querySelector(".content-wrapper");
+var FEEDBACK_WRAPPER_ID = "feedback-wrapper";
 
-openZenMode(content);
-formatContent(wrapper);
+
+/*
+ * Open zen mode
+ */
+(function(content) {
+    var classes = content.className.split(' ');
+    if(classes.indexOf("zen-mode") != -1)
+        return;
+    document.querySelector(".open-zen-mode").click();
+})(content);
+
+
+/*
+ * Format content
+ */
+// We need to use `el.setAttribute()` instead of `el.style.property`
+// because there is an "!important" CSS rule that we need to override.
+wrapper.setAttribute("style", (
+    "width: 45%;" +
+    "margin: 0px 30px !important;" +
+    "display: inline-block;" +
+    "vertical-align: top;"
+));
+
+
+/*
+ * Create feedback wrapper
+ */
+(function(content) {
+    if(content.querySelector("#" + FEEDBACK_WRAPPER_ID))
+        return;
+
+    var div = document.createElement(FEEDBACK_WRAPPER_ID);
+    div.id = FEEDBACK_WRAPPER_ID;
+    div.style.display = "inline-block";
+    div.style.verticalAlign = "top";
+    div.style.width = "45%";
+    content.appendChild(div);
+})(content);
+
+
+/*
+ * Create feedback areas 
+ */
+function createFeedbackArea(wrapper, section) {
+    var id = section.id + "-feedback";
+    // We use this syntax because ids contain numbers
+    if(wrapper.querySelector("[id='" + id + "']"))
+        return;
+
+    var div = document.createElement("div");
+    div.id = id;
+
+    var label = document.createElement("div");
+    label.innerHTML = section.title;
+    label.style.fontWeight = "bold";
+    div.appendChild(label);
+
+    var textarea = document.createElement("textarea");
+    div.appendChild(textarea);
+
+    wrapper.appendChild(div);
+}
+
+var feedbackWrapper = document.getElementById(FEEDBACK_WRAPPER_ID);
+var sections = (function() {
+    var titles = document.querySelectorAll("h2:not(.subtitle)");
+    var sections = {};
+    var link;
+    for(title of titles) {
+        link = title.querySelector("a");
+        sections[title.id] = {
+            id: title.id,
+            title: readText(link),
+            url: link.href,
+        };
+    }
+    return sections;
+})();
+
+for(var id in sections) {
+    createFeedbackArea(feedbackWrapper, sections[id]); 
+}
