352f3b9513561a7f977b021858af51cdc9e24d73
Vincent Lefoulon <vincent.lefoulon@free.fr>
2018-12-01 16:13:17 +0000

    Use basic database for annotations

--- annotator/server.py
+++ annotator/server.py
@@ -1,10 +1,11 @@
+import shelve
 import uuid
 
 from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
-database = dict()
+db_path = "database/annotations"
 
 
 @app.route("/")
@@ -20,7 +21,8 @@ def updated():
 @app.route("/search")
 def search():
     page_id = request.args["page"]
-    annotations = [ann for ann in database.values() if ann["page"] == page_id] 
+    with shelve.open(db_path) as db:
+        annotations = [ann for ann in db.values() if ann["page"] == page_id] 
     return jsonify({
         "total": len(annotations),
         "rows": annotations,
@@ -30,25 +32,29 @@ def search():
 @app.route("/create", methods=["POST"])
 def create():
     annotation = request.get_json()
-    annotation_id = uuid.uuid4()
-    database[annotation_id] = annotation 
+    annotation_id = str(uuid.uuid4())
+    with shelve.open(db_path) as db:
+        db[annotation_id] = annotation 
     return jsonify({"status": "success", "id": annotation_id})
 
 
 @app.route("/update/<int:annotation_id>", methods=["POST"])
 def update(annotation_id):
     annotation = request.get_json()
-    database[annotation_id] = annotation 
+    with shelve.open(db_path) as db:
+        db[annotation_id] = annotation 
     return jsonify({"status": "success"})
 
 
 @app.route("/delete/<int:annotation_id>", methods=["POST"])
 def delete(annotation_id):
-    try:
-        database.pop(annotation_id)
-    except KeyError:
-        return jsonify({"status": "error"})
-    return jsonify({"status": "success"})
+    status = "success"
+    with shelve.open(db_path) as db:
+        try:
+            db.pop(annotation_id)
+        except KeyError:
+            status = "error" 
+    return jsonify({"status": status})
 
 
 if __name__ == "__main__":
